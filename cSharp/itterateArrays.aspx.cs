f8ee64f0c10d32f3166c4095ccdb0ec0a6d36b83
SimonC <sonny@aegautosales.com>
2017-04-23 20:59:41 -0500

    loop through arrays

---
+++ cSharp/itterateArrays.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cSharp
+{
+    public partial class itterateArrays : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] names = new string[] {"Company","Manager","Employees","Products","Marketing", "Pricing" };
+
+            for (int i = 0; i < names.Length; i++) 
+            {
+                string lastName = names[i];
+                resultLabel.Text += lastName +" -- " + names[i];
+            }
+        }
+    }
+}
