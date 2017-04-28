d0eb894721f4352c4be75037d3ef7e45854b6aca
SimonC <sonny@aegautosales.com>
2017-04-28 18:37:58 -0500

    Refactoring...

--- cSharp/helperMethod.aspx.cs
+++ cSharp/helperMethod.aspx.cs
@@ -14,7 +14,7 @@ namespace cSharp
 
         }
 
-        private void calculateCups()
+        private void calculateCups(double measureToCupRatio, string measureName)
         {
             if(quantityTextBox.Text.Trim().Length == 0)
                 return;
@@ -23,40 +23,38 @@ namespace cSharp
             if (!double.TryParse(quantityTextBox.Text, out quantity)) 
                 return;
 
-            double cups = 0.0;
-            if (fromCupsRadio.Checked) cups = quantity;
-            else if (fromPintsRadio.Checked) cups = quantity * 2;
-            else if (fromQuartsRadio.Checked) cups = quantity * 4;
-            else if (fromGallonsRadio.Checked) cups = quantity * 16;
+            double cups = quantity * measureToCupRatio;
 
-            resultLabel.Text = "The number of cups: " + cups.ToString();
+            resultLabel.Text = string.Format(
+                                "{0:N2} {1} is equal to {2:N2} cups.",quantity, measureName, cups
+                                 );
 
             
         }
 
         protected void quantityTextBox_TextChanged(object sender, EventArgs e)
         {
-            calculateCups();
+            //calculateCups();
         }
 
         protected void fromCupsRadio_CheckedChanged(object sender, EventArgs e)
         {
-            calculateCups();
+            calculateCups(1.0, "cups");
         }
 
         protected void fromQuartsRadio_CheckedChanged(object sender, EventArgs e)
         {
-            calculateCups();
+            calculateCups(2.0, "quarts");
         }
 
         protected void fromPintsRadio_CheckedChanged(object sender, EventArgs e)
         {
-            calculateCups();
+            calculateCups(4.0, "pints");
         }
 
         protected void fromGallonsRadio_CheckedChanged(object sender, EventArgs e)
         {
-            calculateCups();
+            calculateCups(16.0, "gallons");
         }
     }
 }
